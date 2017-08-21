a036640693c9b5e8aceb99f9bac0ac51de07c69f
Adnanamin <adnan.amin@hotmail.com>
2017-08-21 15:32:41 +0500

    SharePoint script
    file update

---
+++ Change Site logo/ChangesiteLogo(SharePoint Onpremise).ps1
@@ -0,0 +1,13 @@
+﻿if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) 
+{
+    Add-PSSnapin "Microsoft.SharePoint.PowerShell"
+}
+
+$sitename= "http://Sp2016"
+$sitelogo= "/siteAssets/NewLogo.jpg"
+$site=new-object Microsoft.SharePoint.SPSite($sitename)
+foreach($web in $site.Allwebs) {
+    $web.SiteLogoUrl=$sitelogo
+    $web.Update()
+}
+$site.Dispose() 
