4c2313b58836f998bdf86d81287119e69db3d74a
Adnanamin <adnan.amin@hotmail.com>
2017-08-22 09:16:34 +0500

    Get List inventory
    This powershell script is useful for fetching the list item count for all list and libraries in a site collection under all sub sites

---
+++ GetList Inventory/GetList Inventory.ps1
@@ -0,0 +1,43 @@
+﻿if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) 
+{
+    Add-PSSnapin "Microsoft.SharePoint.PowerShell"
+}
+
+function GetListInventory($siteUrl)
+{
+    $webApp = Get-SPWebApplication $siteUrl
+   
+    $SiteDetail = @();
+
+    Foreach ($web in $webApp | Get-SPSite -Limit All | Get-SPWeb -Limit All)
+    {
+       Write-host "Processing site $web.Name..."
+
+            foreach($list in $web.lists)
+            { 
+             $row = new-object PSObject
+                Add-member -inputObject $row -memberType NoteProperty -Name "Site Name" -value $web.Name
+                Add-member -inputObject $row -memberType NoteProperty -Name "URL" -value $web.Url
+                Add-member -inputObject $row -memberType NoteProperty -Name "List Title" -value $List.Title
+                Add-member -inputObject $row -memberType NoteProperty -Name "List Item Count" -value $list.Items.Count 
+                Add-member -inputObject $row -memberType NoteProperty -Name "Last Modified Date" -value $List.LastItemModifiedDate
+                if ($list.EnableVersioning -eq $TRUE) 
+                {
+                Add-member -inputObject $row -memberType NoteProperty -Name "Versioning" -value "Yes"
+                }else
+                {
+                Add-member -inputObject $row -memberType NoteProperty -Name "Versioning" -value "No"
+                
+                } 
+                $SiteDetail += $row;
+
+            }
+
+        
+
+    }
+    $SiteDetail 
+}
+
+GetListInventory "http://mxwl-sp01" | Out-GridView 
+#GetListInventory "http://mxwl-spa01" |  Out-File "E:\FMT\ListInventory.csv"
