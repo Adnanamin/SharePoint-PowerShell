0430f772be8935f538b92518f2dd8bd58b3d3bb0
Adnanamin <adnan.amin@hotmail.com>
2017-09-12 17:51:41 +0500

    Show or Hid list and libraries
    New PowerShell script to Show or Hid list and libraries

---
+++ Show or Hide List and libraries/ShowHideList(SharePoint Onpremise).ps1
@@ -0,0 +1,23 @@
+﻿
+function ShowHideList($siteUrl, $listName, $showHideValue)
+{
+    $web = Get-SPWeb $siteUrl
+    #Get the List
+    $list = $web.Lists[$listName]
+ 
+    #Set the Hidden Property to show or hide
+    $list.Hidden = $showHideValue
+    $list.Update()
+
+    if ($showHideValue -eq $true)
+        {
+            Write-Host "$listName hidden successfully!"
+        }else{
+            Write-Host "$listName enabled/shown successfully!"
+        }
+    $web.Dispose()
+}
+
+#function call
+#site URL, List/Library name, $true/$false
+ShowHideList "https://sharepoint2016" "Internal Documents" $true
